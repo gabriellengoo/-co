dd9c88390cfc9658d047c31fa42ad654b2f31988
admin <admin@MacBook-Pro.local>
2025-03-28 16:40:15 +0000

    first commit

--- fe/pages/hometemp.vue
+++ fe/pages/hometemp.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h1>Welcome to Nuxt Spotify</h1>
-      <button @click="login">Login with Spotify</button>
+      <!-- <button @click="login">Login with Spotify</button> -->
       <!-- <div v-if="user">
         <h2>Hello, {{ user.display_name }}</h2>
         <p>Email: {{ user.email }}</p>
--- fe/store/auth.js
+++ fe/store/auth.js
@@ -1,33 +1,33 @@
-export const state = () => ({
-    token: null,
-    user: null,
-  })
+// export const state = () => ({
+//     token: null,
+//     user: null,
+//   })
   
-  export const mutations = {
-    setToken(state, token) {
-      state.token = token
-    },
-    setUser(state, user) {
-      state.user = user
-    },
-  }
+//   export const mutations = {
+//     setToken(state, token) {
+//       state.token = token
+//     },
+//     setUser(state, user) {
+//       state.user = user
+//     },
+//   }
   
-  export const actions = {
-    async login({ commit }) {
-      const config = useRuntimeConfig()
-      const redirectUri = config.public.spotifyRedirectUri
-      const clientId = config.public.spotifyClientId
-      const scope = 'user-read-private user-read-email'
-      const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}`
-      window.location.href = authUrl
-    },
+//   export const actions = {
+//     async login({ commit }) {
+//       const config = useRuntimeConfig()
+//       const redirectUri = config.public.spotifyRedirectUri
+//       const clientId = config.public.spotifyClientId
+//       const scope = 'user-read-private user-read-email'
+//       const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scope}`
+//       window.location.href = authUrl
+//     },
   
-    async fetchUser({ commit, state }) {
-      if (!state.token) return
-      const { data } = await useFetch('https://api.spotify.com/v1/me', {
-        headers: { Authorization: `Bearer ${state.token}` },
-      })
-      commit('setUser', data)
-    },
-  }
+//     async fetchUser({ commit, state }) {
+//       if (!state.token) return
+//       const { data } = await useFetch('https://api.spotify.com/v1/me', {
+//         headers: { Authorization: `Bearer ${state.token}` },
+//       })
+//       commit('setUser', data)
+//     },
+//   }
   
